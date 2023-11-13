a1d4cc46ab44d1c70650e3d477e2c70a24c8d8eb
Shreyansh Shukla <shreyansh.14010@gmail.com>
2023-11-13 11:23:33 +0530

    initial commit and tailwind setup

---
+++ youtube_react/notes.md
@@ -0,0 +1,3 @@
+### Higher Order Component
+
+### Debouncing
---
+++ youtube_react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
